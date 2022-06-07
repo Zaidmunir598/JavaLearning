6915b547029130dc53ff8c8a36b2ac3cc3f1aa90
Zaidmunir598 <105710116+Zaidmunir598@users.noreply.github.com>
2022-06-07 02:21:41 -0700

    added all the files

---
+++ MainDemoBox.java
@@ -0,0 +1,25 @@
+class Box
+{
+	double width,height,depth;
+	Box(double w,double h,double d);
+	{
+		width=w;
+		height=h;
+		depth=d;
+	}
+	double volume()
+	{
+		return width*height*depth;
+	}
+	class MainDemoBox
+	{
+		public static void main(String Args[])
+		{
+			double a;
+			Box obj1=new Box(2,3,4);
+			Box obj2=new Box(2.5,5,6.5);
+			a = obj.volume();
+			System.out.println(a);
+		}
+	}
+}
